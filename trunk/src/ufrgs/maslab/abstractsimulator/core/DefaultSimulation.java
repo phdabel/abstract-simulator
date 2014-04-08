b01fe5254bf2621cc9a93222c3b6cb136cb89a94
phd.abel <phd.abel@6494268f-6859-4493-4906-5f7b984a2ba3>
2014-04-08 06:44:55 +0000

    updating simulator

--- trunk/src/ufrgs/maslab/abstractsimulator/core/DefaultSimulation.java
+++ trunk/src/ufrgs/maslab/abstractsimulator/core/DefaultSimulation.java
@@ -1,40 +1,8 @@
 package ufrgs.maslab.abstractsimulator.core;
 
-
-
 public abstract class DefaultSimulation {
 	
 	
-	/**
-	 * environment variable
-	 */
-	//private Environment<Val,Var> env = new Environment<Val, Var>();
-
-	/**
-	 * 
-	 * @return
-	 */
-	//public Environment<Val,Var> getEnv() {
-	//	return env;
-	//}
-
-	/**
-	 * 
-	 * @param env
-	 */
-	//public void setEnv(Environment<Val,Var> env) {
-	//	this.env = env;
-	//}
 	
 	
-	/**
-	private int generatedTasks = 0;
-	
-	private int time = 1;
-	
-	*/
-	
-	
-	
-
 }
